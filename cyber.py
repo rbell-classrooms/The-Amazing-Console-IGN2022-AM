eb878684e4322e3ec266079a5dfcc2a733e0fd0e
Redsnow12 <Snowb@bentonvillek12.org>
2022-11-11 12:04:34 -0600

    Created user interface for cyber modules

--- cyber.py
+++ cyber.py
@@ -1,8 +1,64 @@
-class Character:
+import time
+
+import art
+import os
+
+
+class CyberCharacterCreator:
+    running = True
     def __init__(self):
-        pass
+        self.characters = []
+
+    def menu(self):
+        #  Display menu
+        art.tprint("Cyber People")
+        print("Options:")
+        print("1. Create Character")
+        print("2. Describe Character")
+        print("3. List Characters")
+        print("Type 1-3 in the console to launch a prompt, or 0 to quit")
+        # Menu Display End
+
+        # user input
+        user_in = input("I Choose: ")
+
+        if (user_in.isnumeric):
+            if user_in == "1":
+                # your main function here!!!
+                pass
+            elif user_in == "2":
+                # your main function here!!!
+                pass
+            elif user_in == "3":
+                # your main function here!!!
+                pass
+            elif user_in == "4":
+                # your main function here!!!
+                pass
+            elif user_in == "5":
+                # your main function here!!!
+                pass
+            elif user_in == "6":
+                # your main function here!!!
+                pass
+            elif user_in == "7":
+                # your main function here!!!
+                pass
+            elif user_in == "8":
+                # your main function here!!!
+                pass
+            elif user_in == "9":
+                # your main function here!!!
+                pass
+            elif user_in == "0":
+                self.running = False
+            else:
+                time.sleep(3)
+                print("incorrect input!")
+                return None
 
     def run(self):
-        pass
-
-
+        self.running = True
+        while self.running:
+            os.system('cls')
+            self.menu()
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 import cyber
 
 # initialize Objects
-cyber_game = cyber.Character()
+cyber_game = cyber.CyberCharacterCreator()
+
 
 # Menu text for selection
 def getMenu():
@@ -19,7 +20,8 @@ def getMenu():
     print("8. Ladybug")
     print("Type 1-8 in the console to launch a prompt, or 0 to quit")
 
-#---------- Main progression starts here ----------
+
+# ---------- Main progression starts here ----------
 
 # Title card
 art.tprint("Welcome to The Amazing Console!")
@@ -30,39 +32,39 @@ time.sleep(3)
 
 # Give options
 end = 0
-while(end != 1):
+while (end != 1):
     getMenu()
     user_in = input("I Choose: ")
 
-    if(user_in.isnumeric):
-            if user_in == "1":
-                # your main function here!!!
-                cyber_game.run()
-            elif user_in == "2":
-                # your main function here!!!
-                pass
-            elif user_in == "3":
-                # your main function here!!!
-                pass
-            elif user_in == "4":
-                # your main function here!!!
-                pass
-            elif user_in == "5":
-                # your main function here!!!
-                pass
-            elif user_in == "6":
-                # your main function here!!!
-                pass
-            elif user_in == "7":
-                # your main function here!!!
-                pass
-            elif user_in == "8":
-                # your main function here!!!
-                pass
-            elif user_in == "9":
-                # your main function here!!!
-                pass
-            else:
-                end = 1
-                print("Goodbye!")
-                pass
+    if (user_in.isnumeric):
+        if user_in == "1":
+            # your main function here!!!
+            cyber_game.run()
+        elif user_in == "2":
+            # your main function here!!!
+            pass
+        elif user_in == "3":
+            # your main function here!!!
+            pass
+        elif user_in == "4":
+            # your main function here!!!
+            pass
+        elif user_in == "5":
+            # your main function here!!!
+            pass
+        elif user_in == "6":
+            # your main function here!!!
+            pass
+        elif user_in == "7":
+            # your main function here!!!
+            pass
+        elif user_in == "8":
+            # your main function here!!!
+            pass
+        elif user_in == "9":
+            # your main function here!!!
+            pass
+        else:
+            end = 1
+            print("Goodbye!")
+            pass
