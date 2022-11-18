f48aff97e6700920151ac8a4a91d33a0d49bc836
rbell01 <110845556+rbell01@users.noreply.github.com>
2022-11-18 08:58:14 -0600

    PUT YOUR CODE IN A NEW FILE
    Thank you

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # import mycode.py
 import art
 import time
+import fish
 
 # Menu text for selection
 def getMenu():
@@ -27,33 +28,6 @@ time.sleep(3)
 # Give options
 end = 0
 
-
-def fishFactoid():
-    print("\n\nWelcome to Fish Facts-oids!")
-    print("By Jack Hodge, Walter Medlin, SPIES")
-    print("With special support from the royal society for the prevention of birds.")
-    print("Choose a fish to learn about!")
-    print(" 1. Tuna \n 2. Swordfish \n 3. Bass \n 4. Carp \n 5. Clownfish \n 6. Shark")
-    userInput = int(input())
-    print("once again, welcome!!!!\n\n")
-    print("welcome to fish fact")
-    if userInput == 1:
-        print("TRIGGER WARNING: VIOLINS: \n\n\n Tuna are well-known in india for being especially aggressive against children. \n In 2017 alone, Tuna killed 1800 children.")
-    elif userInput == 2:
-        print("Swordfish are an endangered species of fish living exclusively in deep-sea thermal vents. They can \n only survive in temperatures of 2000 Celcius or above.")
-    elif userInput == 3:
-        print("The average lifespan of a large-mouth bass is 16 years \n The bass is also a name for an instrument.")
-    elif userInput == 4:
-        print("Carp are famous for being the Magikarp from Pokemon \n Carp are very colorful creatures, their scales are often confused with mermaids!")
-    elif userInput == 5:
-        print("Clownfish are renowned for their ability to tell incredible jokes. \n In switzerland, national festivals are held surrounding the 'joke of the year', \n told by a local clownfish.")
-    elif userInput == 6:
-        print("There are over a 1000 species of shark \n Most sharks are cold blooded \n Sharks are apex predators")
-    else:
-        print("Not a valid option, please re-run the program and choose again.")
-
-    print("fish fact. welcome1!")
-
 while(end != 1):
     getMenu()
     user_in = input("I Choose: ")
@@ -75,7 +49,7 @@ while(end != 1):
                 # your main function here!!!
                 pass
             elif user_in == "6":
-                fishFactoid()
+                fish.fishFactoid()
             elif user_in == "7":
                 # your main function here!!!
                 pass
