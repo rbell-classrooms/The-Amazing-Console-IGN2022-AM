66eb39d473ea1f48ff26bdfdaa2b66bae2725f88
AbrielGanv <gabrielvang49@gmail.com>
2022-11-18 08:35:41 -0600

    Hangman Letter Guess

--- Secret.py
+++ Secret.py
@@ -16,7 +16,26 @@ def secret():
 
     guessed = False
 
-    print(word)
+    print()
+
+    while (guessed == False) and tries > 0:
+        print ('You have' + str(tries) + ' tries' )
+        guess = input('Guess a letter').lower()
+        #User inputs
+        if guess in letters_guessed:
+            print('You have already guessed that letter, try again.')
+        elif (guess not in word):
+            print('That letter is not in the word')
+            letters_guessed.append(guess)
+            tries -= 1
+        elif guess in word:
+            print('That letter is in the word')
+            letters_guessed.append(guess)
+        else:
+            print('You entered an incorrect input')
+
+
+
 
 
 def get_word():
