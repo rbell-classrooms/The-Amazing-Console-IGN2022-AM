9d115f739331f6ef240b6081ffcf23d88aa3e22e
Redsnow12 <Snowb@bentonvillek12.org>
2022-11-18 08:18:45 -0600

    Added character data

---
+++ CyberData.py
@@ -0,0 +1,13 @@
+#  Character data for Cyber
+races = {"cyber": "1", "cyborg": "2", "Hybrid": "2", "enhanced": "3", "human": "4"}
+
+weapons = ["Space Blaster", "Vorpal Sword", "Gravity Hammer", "Rail-Gun", "Electric Bass"]
+
+armor = ["Plasma Armor", "Nano Armor", "Fiber Armor", "Smart Armor", "Clone Armor"]
+
+style = ["High end tech", "CyberPunk", "SteamPunk", "Retro-Futurism"]
+
+cyberware = ["H-OS", "Cy-Legs", "Compound-Arms", "MEM-Booster", "Cha-OS"]
+
+skill = ["Charm", "Detect Thoughts", "Mending", "Shocking Grasp", "Haste", "Burning Stride", "Grease", "Heat Metal",
+         "Identify", "Invisibility"]
