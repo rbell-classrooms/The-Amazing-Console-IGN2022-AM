ee585b471a1d734d3664a75659f0550d296184fc
AbrielGanv <gabrielvang49@gmail.com>
2022-11-18 08:59:03 -0600

    Hangman Letter Input

--- Secret.py
+++ Secret.py
@@ -20,19 +20,31 @@ def secret():
 
     while (guessed == False) and tries > 0:
         print ('You have' + str(tries) + ' tries' )
-        guess = input('Guess a letter').lower()
+        guess = input('Guess a letter or enter the full word').lower()
         #User inputs
-        if guess in letters_guessed:
-            print('You have already guessed that letter, try again.')
-        elif (guess not in word):
-            print('That letter is not in the word')
-            letters_guessed.append(guess)
-            tries -= 1
-        elif guess in word:
-            print('That letter is in the word')
-            letters_guessed.append(guess)
-        else:
-            print('You entered an incorrect input')
+        if len(guess) == 1:
+
+            if guess in letters_guessed:
+                print('You have already guessed that letter, try again.')
+            elif (guess not in word):
+                print('That letter is not in the word')
+                letters_guessed.append(guess)
+                tries -= 1
+            elif guess in word:
+                print('That letter is in the word')
+                letters_guessed.append(guess)
+            else:
+                print('You entered an incorrect input')
+        elif len(guess) == len(word):
+            if guess == word():
+                print('Good job, you guessed the correct word')
+                guessed == True
+            else:
+                print('That is the wrong word')
+                guessed == False
+                tries -=1
+
+
 
 
 
