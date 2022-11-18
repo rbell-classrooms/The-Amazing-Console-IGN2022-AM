27ba25ddf829a3a8994e5c2f38fd4e90c68d3df4
AbrielGanv <gabrielvang49@gmail.com>
2022-11-18 08:01:16 -0600

    Test File
    Test File

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 #boop
+
+import turtle
+trtl = turtle.Turtle()
+#Secret = 5
+
+
+def secret():
+    print('what walks on 6 legs, has two eyes, and loves flowers?')
+    guess = input(':''')
+    print('you guessed ', {guess})
+    if (guess != 'Ladybugs'):
+        print('try again')
+
+while True:
+    secret()
