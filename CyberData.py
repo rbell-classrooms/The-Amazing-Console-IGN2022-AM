719aa7b57823b0720d1c129389ad52d42bc354a8
Redsnow12 <Snowb@bentonvillek12.org>
2023-02-20 09:10:00 -0600

    Update CyberData.py

--- CyberData.py
+++ CyberData.py
@@ -1,13 +1,15 @@
 #  Character data for Cyber
-races = {"cyber": "1", "cyborg": "2", "Hybrid": "2", "enhanced": "3", "human": "4"}
+#  selections key: [index, Description]
+races = {"Cyborg": ["1"], "Hybrid": ["2"], "Human": ["3"]}
 
-weapons = ["Space Blaster", "Vorpal Sword", "Gravity Hammer", "Rail-Gun", "Electric Bass"]
+style = {"High-end Tech": ["1"], "CyberPunk": ["2"], "SteamPunk": ["3"], "Retro-Futurism": ["4"]}
 
-armor = ["Plasma Armor", "Nano Armor", "Fiber Armor", "Smart Armor", "Clone Armor"]
+#  attributes key: [attribute values, Description] strength, int, dex, wis, chr
+weapons = {"Space Blaster": [3, 8, 5, 5, 1, "A basic laser pistol, best for those with a strong mind"], "Vorpal Sword": [5, 5, 9, 3, 1, "A powerful vibrating blade, best for those who are physically quick"], "Gravity Hammer": [9, 1, 3, 2, 0, "Default Description"], "Rail-Gun": [5, 4, 5, 8, 3, ""], "Electric Bass guitar": [2, 5, 5, 4, 8, ""]}
 
-style = ["High end tech", "CyberPunk", "SteamPunk", "Retro-Futurism"]
+armor = {"Plasma Armor": [2, 7, 4, 3, 2, ""], "Nano Armor": [1, 7, 3, 5, 2, ""], "Fiber Armor": [6, 4, 3, 3, 3, ""], "Smart Armor": [5, 3, 3, 5, 2, ""], "Clone Armor": [4, 3, 5, 3, 8, ""]}
 
-cyberware = ["H-OS", "Cy-Legs", "Compound-Arms", "MEM-Booster", "Cha-OS"]
+cyberware = {"H-OS": [0, 0, 0, 0, 0, ""], "Cy-Legs": [0, 0, 0, 0, 0, ""], "Compound-Arms": [0, 0, 0, 0, 0,""], "MEM-Booster": [0, 0, 0, 0, 0, ""], "Cha-OS": [0, 0, 0, 0, 0, ""]}
 
-skill = ["Charm", "Detect Thoughts", "Mending", "Shocking Grasp", "Haste", "Burning Stride", "Grease", "Heat Metal",
-         "Identify", "Invisibility"]
+skill = {"Charm": [0, 0, 0, 0, 0, ""], "Detect Thoughts": [0, 0, 0, 0, 0, ""], "Mending": [0, 0, 0, 0, 0, ""], "Shocking Grasp": [0, 0, 0, 0, 0, ""], "Haste": [0, 0, 0, 0, 0, ""], "Burning Stride": [0, 0, 0, 0, 0, ""],
+         "Grease": [0, 0, 0, 0, 0, ""], "Heat Metal": [0, 0, 0, 0, 0, ""], "Identify": [0, 0, 0, 0, 0, ""], "Invisibility": [0, 0, 0, 0, 0, ""]}
